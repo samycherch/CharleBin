69af289579281516b4ca9aa9f9a67506b9d97ffe
Samy Cherchari <samy.cherchari6@etu.univ.lorraine.fr>
2026-02-13 08:48:03 +0100

    Ajout d'une durée d'expiration

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -65,6 +65,7 @@ class Configuration
         'expire_options' => array(
             '5min'   => 300,
             '10min'  => 600,
+            '30min'  => 1800,
             '1hour'  => 3600,
             '1day'   => 86400,
             '1week'  => 604800,
