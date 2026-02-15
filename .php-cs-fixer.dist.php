d157a347f6f50850711c8054cd31d8160fa02a56
Samy Cherchari <samy.cherchari6@etu.univ.lorraine.fr>
2026-02-15 21:39:29 +0100

    modification du fichier

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,19 @@
 <?php
+
 $finder = (new PhpCsFixer\Finder())
-    ->in(DIR)
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->exclude('data')
+    ->exclude('log')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PER-CS' => true,
-        '@PHP8x2Migration' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => true,
     ])
     ->setFinder($finder)
 ;
